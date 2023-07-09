d60be93bc71f0a7bf3e68ec3f3352fd863538883
Kittycat <sankhayan2002@gmail.com>
2023-07-09 23:29:17 +0530

    fix: now large data can be importted

--- src/Code.js
+++ src/Code.js
@@ -6,10 +6,7 @@ function findAll(data) {
   for (var i = 0; i < values.length; i++) {
     for (var j = 0; j < values[0].length; j++) {
       if (values[i][j] == data) {
-        //sheet.getActiveRange().getCell(i+1,j+1).setValue(newdata);
         sheet.getActiveRange().getCell(i+1,j+1).setBackground("red");
-        //sheet.getRange(i+1,j+1).setValue(newdata);
-        //sheet.getRange(i + 1, j + 1).setBackground("red");
       }
     }
   }
@@ -19,13 +16,18 @@ function changeDate(){
   var sheet = SpreadsheetApp.getActiveSheet();
   const range = sheet.getActiveRange();
   const values = range.getValues();
-  for (var i = 0; i < values.length; i++) {
-    for (var j = 0; j < values[0].length; j++) {
-      
-        sheet.getActiveRange().setBackground("red")
-      
-    }
-  }
+  var data = {
+    "data":JSON.stringify(values),
+    "query":"honk"
+  };
+  var payload = JSON.stringify(data);
+  var options = {
+    "method" : "POST",
+    "contentType" : "application/json",
+    "payload" : payload
+  };
+  var r=UrlFetchApp.fetch("https://datawhisperer.fly.dev/query",options)
+  return r.getContentText()
 }
 
 function findAllnReplace(data,newdata) {
@@ -38,8 +40,6 @@ function findAllnReplace(data,newdata) {
       if (values[i][j] == data) {
         sheet.getActiveRange().getCell(i+1,j+1).setValue(newdata);
         sheet.getActiveRange().getCell(i+1,j+1).setBackground("red");
-        //sheet.getRange(i+1,j+1).setValue(newdata);
-        //sheet.getRange(i + 1, j + 1).setBackground("red");
       }
     }
   }
@@ -49,13 +49,12 @@ function findAllnReplace(data,newdata) {
 function importCSVFromGoogleDrive(filename) {
   var file = DriveApp.getFilesByName(filename).next();
   var csvData = Utilities.parseCsv(file.getBlob().getDataAsString());
-  var sheet = SpreadsheetApp.getActiveSheet();
-  var startRow = sheet.getDataRange().getValues();
-  //some blek magic to get the length since length in google sheets start from 1
-  //needed for appending data
-  var length = startRow.length > 1 ? startRow.length + 1 : 1;
+  var ss=SpreadsheetApp.getActive();
+  var sheet = ss.getActiveSheet();
   //append data
-  sheet.getRange(length, 1, csvData.length, csvData[0].length).setValues(csvData);
+  //using spread sheets api for zuper fast appending
+  Sheets.Spreadsheets.Values.append({values:csvData},ss.getId(),sheet.getSheetName(),{valueInputOption: "USER_ENTERED"});
+ 
 }
 
 function importCSVFromWeb(url) {
